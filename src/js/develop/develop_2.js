ad43f711961749a6f4bef9f4ed45ebf6cdc81129
Art G <artem.garmatyk@gmail.com>
2016-05-03 10:53:04 +0300

    add

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -9,7 +9,40 @@ $(document).ready(function(){
             type: $(this).data("type")
         });
         return false;
-    })
+    });
+    
+    
+    setTimeout(function() {
+        $('select').styler();
+    }, 100)
+    
+    var timeM = 60;
+    var timeS = 20;
+    
+    setInterval(function(){
+        if(timeS == 0) {
+            timeS = 59;
+            timeM = timeM - 1;
+            
+            if (timeM < 10) {
+                $('.minutes').html('0'+ timeM);
+            } else {
+                $('.minutes').html(timeM);
+            } 
+            
+        } else {
+            timeS = timeS - 1;
+        }
+        
+        if (timeS < 10) {
+            $('.seconds').html('0'+ timeS);
+        } else {
+            $('.seconds').html(timeS);
+        }
+        
+          
+   
+    }, 1000)
     
 });
 
