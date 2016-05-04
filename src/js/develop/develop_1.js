35c94ede87c167a83cc017a2ff70d60d23c48184
Dmitry <dmitry1939@gmail.com>
2016-05-04 11:03:32 +0300

    circle checkpoint

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,7 +1,30 @@
+/* prety circle popup toggle */
 
+    function pretyCirclePopupToggle(){
+
+        $('.prety-circle-title').hover(
+            function(){
+                $('.prety-circle-main-wrap .prety-circle-item').eq($(this).parents('.prety-circle-item').index()).addClass('hovered');
+                $(this).parents('.prety-circle-item').addClass('hovered');
+                $(this).find('.prety-circle-popup').fadeIn(300);
+
+            },
+            function(){
+                $('.prety-circle-main-wrap .prety-circle-item').eq($(this).parents('.prety-circle-item').index()).removeClass('hovered');
+                $(this).parents('.prety-circle-item').removeClass('hovered');
+                $(this).find('.prety-circle-popup').fadeOut(300);
+
+            }
+        );
+
+    }
+
+/* /prety circle popup toggle */
 
 
 $(document).ready(function(){
+
+    pretyCirclePopupToggle();
 
 });
 
