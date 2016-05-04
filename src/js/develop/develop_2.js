74b62be5ccbc4116ecb2a51ea5244b1d32861f44
Art G <artem.garmatyk@gmail.com>
2016-05-04 08:06:20 +0300

    add some some

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -1,7 +1,57 @@
+function googleMap(mapWrap){
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(cordX,cordY);
+        var myOptions = {
+            zoom: 16,
+            center: myLatlng,
+            disableDefaultUI: false, //без управляющих елементов
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
+            zoomControlOptions: {
+               position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
+            }
+        }
+        var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
 
+
+        //больше - http://map-icons.com/
+        /*/маркер на svg*/
+
+        //var image = 'images/footer-contact-marker.png';   // иконка картинкой
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            animation: google.maps.Animation.DROP, // анимация при загрузке карты
+            //icon: image //  иконка картинкой
+        });
+
+
+        /*анимация при клике на маркер*/
+        marker.addListener('click', toggleBounce);
+        function toggleBounce() {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+        }
+        /*/анимация при клике на маркер*/
+
+        /*По клику открываеться инфоблок*/
+        google.maps.event.addListener(marker, 'click', function() {
+           // infowindow.open(map,marker);
+        });
+
+    }
+    initialize();
+}
 
 
 $(document).ready(function(){
+    
+    googleMap('map');
+    
+    
     $('.grouped_elements').click(function(){
         
         $.fancybox({
@@ -17,7 +67,7 @@ $(document).ready(function(){
     }, 100)
     
     var timeM = 60;
-    var timeS = 20;
+    var timeS = 00;
     
     setInterval(function(){
         if(timeS == 0) {
@@ -42,7 +92,14 @@ $(document).ready(function(){
         
           
    
-    }, 1000)
+    }, 1000);
+    
+    $('.time-picker').wickedpicker({twentyFour: true, title: 'Время звонка'});
+    
+    $( "#datepicker" ).datepicker({
+      firstDay: 1,
+      minDate: 1
+    });
     
 });
 
